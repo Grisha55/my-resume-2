81804b36928ac632d99b9b9b9b540ee52c1f9f21
Grisha55 <grishavinyar@icloud.com>
2026-05-18 14:35:34 +0300

    Created computers page

--- app/[locale]/computers/page.tsx
+++ app/[locale]/computers/page.tsx
@@ -1,5 +1,9 @@
+import { ComputersView } from '@/src/views/computersPage';
+
 export default function ComputersPage() {
 	return (
-		<div>Computers</div>
+		<div className="mt-20">
+			<ComputersView />
+		</div>
 	);
 }
---
+++ src/views/computersPage/index.ts
@@ -0,0 +1,1 @@
+export { ComputersView } from './ui/ComputersPage';
